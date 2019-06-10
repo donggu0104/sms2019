199920a5f55942e4bebc3de052a9cced33f1a7fe
10357 <1035703271@qq.com>
2019-06-10 12:46:30 +0800

    2019年6月10号app02

--- src/main/java/com/briup/apps/app02/conf/WebConfig.java
+++ src/main/java/com/briup/apps/app02/conf/WebConfig.java
@@ -1,12 +1,7 @@
 package com.briup.apps.app02.conf;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
